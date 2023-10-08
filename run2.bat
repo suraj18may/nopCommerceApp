c1c04a32be2c021c55cbb2138dd3708e84e30911
suraj <surajkishorsk@gmail.com>
2023-10-08 18:35:59 +0530

    4rd commit

--- run2.bat
+++ run2.bat
@@ -1,2 +1,3 @@
 pytest -v -s --html=Reports\report4.html testCases\test_SearchCustomerEmail.py --browser chrome
 rem pytest -v -s --html=Reports\report5.html testCases\test_SearchCustomerByName.py --browser chrome
+rem pytest -v -s -m "sanity" --html=Reports\report5.html testCases\test_SearchCustomerByName.py --browser chrome
